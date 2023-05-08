1306f291ee6f423d6ee02b8ef6378ada3d049d49
Endersis <alexisgt7168@gmail.com>
2023-05-08 01:37:31 -0600

    Fixes: login css

--- src/Main/loginPop/login.js
+++ src/Main/loginPop/login.js
@@ -9,30 +9,30 @@ import {
     TextField,
 
   } from "@mui/material";
-
+import './login.css'
   
   const Login= (props)=> {
     const { handleClose, handleSaveDialog, open } = props;
   
     return (
       <>
-        <Dialog open={open} >
-          <DialogTitle className="dialog-content" px={6}>
+        <Dialog open={open} className="dialog" >
+          <DialogTitle  px={6} className='dialog-title' >
              Login
           </DialogTitle>
           <DialogContent mt={-4}>
-            <Box className="box-pop-private">
+            <Box >
         
               <Stack
               spacing={8}
-             
+                mt={4}
                 direction={{ xs: "column"}}
               >
                 <TextField
                   name="Usuario"
                   label="Usuario"
                   rows={4}
-                  variant="filled"
+                  variant="standard"
     
                 />
   
@@ -40,10 +40,10 @@ import {
                   name="Contraseña"
                   label="Contraseña"
                   rows={4}
-                  variant="filled"
+                  variant="standard"
     
                 />
-  ¿
+
               </Stack>
             </Box>
   
@@ -51,7 +51,7 @@ import {
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleSaveDialog}>OK</Button>
+            <Button onClick={handleSaveDialog}>Enviar</Button>
           </DialogActions>
         </Dialog>
       </>
