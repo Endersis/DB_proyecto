3886c522a8f74e2c91607d5a426a313da2fd5f8a
Endersis <alexisgt7168@gmail.com>
2023-05-24 17:56:26 -0600

    ultimo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { ThemeProvider } from "@mui/material";
 import Main from "./Main/main";
 import { theme } from "./theme";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Conocenos from "./conocenos";
 
+
+
 function App() {
+
+  const[registros, setRegistros] = useState([])
+  useEffect(() => {
+    const getRegistros = ()=>{
+        fetch('http://localhost:9000/api')
+        .then(res=> res.json())
+        .then(res=> console.log(res))
+    }
+    getRegistros()
+    
+    },[])
   return (
     <ThemeProvider theme={theme}>
     <Router>
